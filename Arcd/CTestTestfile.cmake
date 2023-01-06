49584dfcf4c17f8123d92c5ad764719f34a02550
whimy <114148910+whimy@users.noreply.github.com>
2023-01-06 16:12:48 +0800

    Add files via upload

---
+++ Arcd/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/whimy030/workspace/arcd
+# Build directory: /home/whimy030/workspace/arcd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("arcd")
+subdirs("examples")
+subdirs("tests")
