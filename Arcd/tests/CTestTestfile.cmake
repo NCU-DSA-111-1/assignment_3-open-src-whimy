28c1e06a60a3f2c8a8857b3199a245fab519aaec
whimy <114148910+whimy@users.noreply.github.com>
2023-01-07 15:50:35 +0800

    Delete Arcd directory

--- Arcd/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/whimy030/workspace/arcd/tests
-# Build directory: /home/whimy030/workspace/arcd/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(codec_tests "/home/whimy030/workspace/arcd/tests/codec_tests")
-set_tests_properties(codec_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/whimy030/workspace/arcd/tests/CMakeLists.txt;10;add_test;/home/whimy030/workspace/arcd/tests/CMakeLists.txt;0;")
